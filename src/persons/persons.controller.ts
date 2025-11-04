17df17fceab413cf965c3b585fd7de434354838b
Renzo Gorosito <renzog6@gmail.com>
2025-11-04 16:25:10 -0300

    refactor(persons): migrate to BaseService

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -1,39 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
-import { PersonsService } from './persons.service';
-import { CreatePersonDto } from './dto/create-person.dto';
-import { UpdatePersonDto } from './dto/update-person.dto';
-import { PaginationDto } from '../common/dto/pagination.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from "@nestjs/common";
+import { ApiQuery, ApiTags } from "@nestjs/swagger";
+import { PersonsService } from "./persons.service";
+import { CreatePersonDto } from "./dto/create-person.dto";
+import { UpdatePersonDto } from "./dto/update-person.dto";
+import { BaseQueryDto } from "../common/dto/base-query.dto";
 
-@ApiTags('persons')
-@Controller('persons')
+@ApiTags("persons")
+@Controller("persons")
 export class PersonsController {
   constructor(private readonly personsService: PersonsService) {}
 
   @Post()
   create(@Body() createPersonDto: CreatePersonDto) {
-    return this.personsService.create(createPersonDto).then((data) => ({ data }));
+    return this.personsService
+      .create(createPersonDto)
+      .then((data) => ({ data }));
   }
 
   @Get()
-  @ApiQuery({ name: 'page', required: false, type: Number })
-  @ApiQuery({ name: 'limit', required: false, type: Number })
-  findAll(@Query() query: PaginationDto) {
+  @ApiQuery({ name: "page", required: false, type: Number })
+  @ApiQuery({ name: "limit", required: false, type: Number })
+  findAll(@Query() query: BaseQueryDto) {
     return this.personsService.findAll(query);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.personsService.findOne(+id).then((data) => ({ data }));
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
-    return this.personsService.update(+id, updatePersonDto).then((data) => ({ data }));
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personsService
+      .update(+id, updatePersonDto)
+      .then((data) => ({ data }));
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.personsService.remove(+id).then((data) => ({ data }));
   }
 }
--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,15 +1,31 @@
 // filepath: sae-backend/src/persons/persons.service.ts
 
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreatePersonDto } from './dto/create-person.dto';
-import { UpdatePersonDto } from './dto/update-person.dto';
-import { PrismaService } from '../prisma/prisma.service';
-import { Gender, MaritalStatus, PersonStatus } from '@prisma/client';
-import { PaginationDto } from '../common/dto/pagination.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreatePersonDto } from "./dto/create-person.dto";
+import { UpdatePersonDto } from "./dto/update-person.dto";
+import { PrismaService } from "../prisma/prisma.service";
+import { BaseService } from "../common/services/base.service";
+import { BaseQueryDto, BaseResponseDto } from "../common/dto/base-query.dto";
+import { Gender, MaritalStatus, PersonStatus } from "@prisma/client";
 
 @Injectable()
-export class PersonsService {
-  constructor(private prisma: PrismaService) {}
+export class PersonsService extends BaseService<any> {
+  constructor(prisma: PrismaService) {
+    super(prisma);
+  }
+
+  protected getModel() {
+    return this.prisma.person;
+  }
+
+  protected buildSearchConditions(q: string) {
+    return [
+      { firstName: { contains: q, mode: "insensitive" } },
+      { lastName: { contains: q, mode: "insensitive" } },
+      { dni: { contains: q, mode: "insensitive" } },
+      { cuil: { contains: q, mode: "insensitive" } },
+    ];
+  }
 
   create(dto: CreatePersonDto) {
     return this.prisma.person.create({
@@ -28,28 +44,16 @@ export class PersonsService {
     });
   }
 
-  async findAll(pagination?: PaginationDto) {
-    const page = pagination?.page ?? 1;
-    const limit = pagination?.limit ?? 10;
-    const [data, total] = await this.prisma.$transaction([
-      this.prisma.person.findMany({
-        skip: (page - 1) * limit,
-        take: limit,
-        include: { employee: true, contacts: true, address: true },
-        orderBy: { createdAt: 'desc' },
-      }),
-      this.prisma.person.count(),
-    ]);
-    return { data, meta: { total, page, limit, totalPages: Math.ceil(total / limit) } };
+  async findAll(
+    query: BaseQueryDto = new BaseQueryDto()
+  ): Promise<BaseResponseDto<any>> {
+    const include = { employee: true, contacts: true, address: true };
+    return super.findAll(query, {}, include);
   }
 
   async findOne(id: number) {
-    const rec = await this.prisma.person.findUnique({
-      where: { id },
-      include: { employee: true, contacts: true, address: true },
-    });
-    if (!rec) throw new NotFoundException(`Person with ID ${id} not found`);
-    return rec;
+    const include = { employee: true, contacts: true, address: true };
+    return super.findOne(id, include);
   }
 
   async update(id: number, dto: UpdatePersonDto) {
@@ -57,23 +61,35 @@ export class PersonsService {
     return this.prisma.person.update({
       where: { id },
       data: {
-        ...(typeof dto.firstName !== 'undefined' ? { firstName: dto.firstName } : {}),
-        ...(typeof dto.lastName !== 'undefined' ? { lastName: dto.lastName } : {}),
-        ...(typeof dto.birthDate !== 'undefined' ? { birthDate: dto.birthDate ? new Date(dto.birthDate as any) : null } : {}),
-        ...(typeof dto.dni !== 'undefined' ? { dni: dto.dni } : {}),
-        ...(typeof dto.cuil !== 'undefined' ? { cuil: dto.cuil } : {}),
-        ...(typeof dto.gender !== 'undefined' ? { gender: dto.gender as Gender | null } : {}),
-        ...(typeof dto.maritalStatus !== 'undefined' ? { maritalStatus: dto.maritalStatus as MaritalStatus | null } : {}),
-        ...(typeof dto.information !== 'undefined' ? { information: dto.information } : {}),
-        ...(typeof dto.status !== 'undefined' ? { status: dto.status as PersonStatus | null } : {}),
+        ...(typeof dto.firstName !== "undefined"
+          ? { firstName: dto.firstName }
+          : {}),
+        ...(typeof dto.lastName !== "undefined"
+          ? { lastName: dto.lastName }
+          : {}),
+        ...(typeof dto.birthDate !== "undefined"
+          ? { birthDate: dto.birthDate ? new Date(dto.birthDate as any) : null }
+          : {}),
+        ...(typeof dto.dni !== "undefined" ? { dni: dto.dni } : {}),
+        ...(typeof dto.cuil !== "undefined" ? { cuil: dto.cuil } : {}),
+        ...(typeof dto.gender !== "undefined"
+          ? { gender: dto.gender as Gender | null }
+          : {}),
+        ...(typeof dto.maritalStatus !== "undefined"
+          ? { maritalStatus: dto.maritalStatus as MaritalStatus | null }
+          : {}),
+        ...(typeof dto.information !== "undefined"
+          ? { information: dto.information }
+          : {}),
+        ...(typeof dto.status !== "undefined"
+          ? { status: dto.status as PersonStatus | null }
+          : {}),
       },
       include: { employee: true, contacts: true, address: true },
     });
   }
 
-  async remove(id: number) {
-    await this.findOne(id);
-    await this.prisma.person.delete({ where: { id } });
-    return { id };
+  async remove(id: number): Promise<void> {
+    await super.remove(id);
   }
 }
