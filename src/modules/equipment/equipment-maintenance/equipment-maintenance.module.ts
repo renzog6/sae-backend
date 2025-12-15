59fca0165f66240bf711b046e89fd6702b50fa5f
Renzo Gorosito <renzog6@gmail.com>
2025-12-15 18:57:58 -0300

    fix: import HistoryModule in EquipmentMaintenanceModule to resolve dependency error

--- src/modules/equipment/equipment-maintenance/equipment-maintenance.module.ts
+++ src/modules/equipment/equipment-maintenance/equipment-maintenance.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { EquipmentMaintenanceService } from "./equipment-maintenance.service";
 import { EquipmentMaintenanceController } from "./equipment-maintenance.controller";
 import { PrismaModule } from "@prisma/prisma.module";
+import { HistoryModule } from "@modules/history/history.module";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, HistoryModule],
     controllers: [EquipmentMaintenanceController],
     providers: [EquipmentMaintenanceService],
     exports: [EquipmentMaintenanceService],
 })
 export class EquipmentMaintenanceModule { }
+
